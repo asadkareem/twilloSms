fe24f89f8702ddd9529769f754c57da053c576d0
Asad-kareem <69818640+asadkareem@users.noreply.github.com>
2023-02-20 06:24:05 -0800

    changes for the deployment

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const smsRouter = require("./routes/sms");
 app.use(express.json({ limit: "10kb" }));
 app.use("/sms", smsRouter);
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Express server listening on port 3000");
 });
